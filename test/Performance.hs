ec6b225106680084683bbbe732a6e163228d25cb
Ryan Reich <ryan.reich@gmail.com>
2017-06-09 19:50:20 -0700

    Add Criterion-based benchmarks.  We are quite competitive!  Seems like BigWord5 is slower than Integer, but the others are not (for inputs of the correct size)

--- test/Performance.hs
+++ test/Performance.hs
@@ -1,8 +1,14 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import Data.BigWord
 import Criterion.Main
+
+makeBigWordsTo 5
 
 main :: IO ()
 main = defaultMain
@@ -11,16 +17,17 @@ main = defaultMain
       [
         bgroup "BigWord"
           [
-            bench "BigWord 0" $ whnf (uncurry (+) @(BigWord 0)) (fromInteger x, fromInteger y)
-            bench "BigWord 1" $ whnf (uncurry (+) @(BigWord 1)) (fromInteger x, fromInteger y)
-            bench "BigWord 2" $ whnf (uncurry (+) @(BigWord 2)) (fromInteger x, fromInteger y)
-            bench "BigWord 3" $ whnf (uncurry (+) @(BigWord 3)) (fromInteger x, fromInteger y)
-            bench "BigWord 4" $ whnf (uncurry (+) @(BigWord 4)) (fromInteger x, fromInteger y)
-            bench "BigWord 5" $ whnf (uncurry (+) @(BigWord 5)) (fromInteger x, fromInteger y)
+            bench "BigWord 0" $ whnf (uncurry $ (+) @(BigWord 0)) (fromInteger x, fromInteger y),
+            bench "BigWord 1" $ whnf (uncurry $ (+) @(BigWord 1)) (fromInteger x, fromInteger y),
+            bench "BigWord 2" $ whnf (uncurry $ (+) @(BigWord 2)) (fromInteger x, fromInteger y),
+            bench "BigWord 3" $ whnf (uncurry $ (+) @(BigWord 3)) (fromInteger x, fromInteger y),
+            bench "BigWord 4" $ whnf (uncurry $ (+) @(BigWord 4)) (fromInteger x, fromInteger y),
+            bench "BigWord 5" $ whnf (uncurry $ (+) @(BigWord 5)) (fromInteger x, fromInteger y)
           ],
         bench "Integer" $ whnf (uncurry (+)) (x, y)
       ]
   ]
 
 twoNumbers :: IO (Integer, Integer)
-twoNumbers = return (1,1)
+twoNumbers = return (x,x)
+  where x = 2^(8 * 2^5) - 1
